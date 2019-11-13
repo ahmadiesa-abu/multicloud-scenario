e7355958683b140acedfea4aecfe821e8d3a845d
Arik Yakir <arikyakir@yahoo.com>
2019-11-13 15:23:00 +0200

    updated version to 5.0.0 release

--- scripts/upload-cloud-infra.sh
+++ scripts/upload-cloud-infra.sh
@@ -3,7 +3,6 @@
 ### This line is required to set the profile
 cfy profiles use localhost -u admin -p admin -t default_tenant >> /tmp/lab_status.txt 2>&1
 
-
 ### public cloud VM Infra
 cfy blueprints upload $blueprint_archive -n $main_file_name -b $id  >> /tmp/lab_status.txt 2>&1
 
